1ecc10d468103b97acd26d5ceb6db3f72b653d57
Tucker <tuckerkloetzke@gmail.com>
2018-05-18 13:27:30 -0500

    Saving genres to store

--- src/reducers/searchCriteriaReducers/genresReducer.js
+++ src/reducers/searchCriteriaReducers/genresReducer.js
@@ -3,16 +3,8 @@ import * as types from "../../constants/actionTypes";
 
 export default function(state = initalState.genres, action) {
   switch (action.type) {
-    case types.ADD_GENRE:
-      return {
-        ...state,
-        genres: [...state.genres, action.genre]
-      };
-    case types.REMOVE_GENRE:
-      return {
-        ...state,
-        genres: state.genres.filter(item => item !== action.genre)
-      };
+    case types.UPDATE_GENRES:
+      return action.genres;
     default:
       return state;
   }
