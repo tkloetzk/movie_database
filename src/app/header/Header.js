d0fd9451840d4a74ea585e15519950c84c40652c
Tucker <tuckerkloetzke@gmail.com>
2018-05-18 14:52:36 -0500

    Eslint and prettier together

--- src/app/header/Header.js
+++ src/app/header/Header.js
@@ -1,35 +1,35 @@
-import React, { Component } from "react";
-import "./Header.css";
+import React, { Component } from 'react';
+import './Header.css';
 
 export default class Header extends Component {
   constructor(...args) {
     super(...args);
     this.state = { height: undefined };
-    this._containerDOM = null;
-    this._scrollPosition = 0;
+    this.containerDOM = null;
+    this.scrollPosition = 0;
     this.onScroll = this.onScroll.bind(this);
     this.scrollAction = 250;
   }
 
   componentDidMount() {
-    window.addEventListener("scroll", this.onScroll);
+    window.addEventListener('scroll', this.onScroll);
   }
 
   onScroll() {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (this.scrollAction >= scrollTop) {
-      const step = this._scrollPosition - scrollTop;
-      const actualHeight = this._containerDOM.offsetHeight;
+      const step = this.scrollPosition - scrollTop;
+      const actualHeight = this.containerDOM.offsetHeight;
       const height = actualHeight + step;
       this.setState({ height });
-      this._scrollPosition = scrollTop;
+      this.scrollPosition = scrollTop;
     }
   }
   render() {
     return (
       <header
         className="header"
-        ref={n => (this._containerDOM = n)}
+        ref={n => (this.containerDOM = n)}
         style={{ height: this.state.height }}
       >
         <div className="header-content display-4">{this.props.children}</div>
