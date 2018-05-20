cc37844b8edae1b3b4fb2cea175228c61b5a0a09
Tucker <tuckerkloetzke@gmail.com>
2018-05-19 23:34:38 -0500

    Streaming services

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import genres from '../searchCriteria/genres/genres-reducer';
+import streamingServices from '../searchCriteria/streaming/streaming-reducer';
+
 // import rootSaga from '../sagas'
 
 const rootReducer = combineReducers({
-  genres
+  genres,
+  streamingServices
 });
 
 const configureStore = () => {
